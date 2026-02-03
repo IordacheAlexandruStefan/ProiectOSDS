2895c26184fb7bb3a80300d22d9a6719102a80b5
Bogdy555 <florescubogdan555@gmail.com>
2026-02-03 17:18:25 +0200

    Done!

--- libhack.py
+++ libhack.py
@@ -1,24 +1,29 @@
-from pwn import ELF
+from elftools.elf.elffile import ELFFile
 
 class libhack:
 
 	def __init__(self, path):
-		self.elf = ELF(path)
 		self.offset = 0
-		self.path = path
+		self.symbols = { }
+		self.data = None
+
+		with open(path, "rb") as f:
+			elf = ELFFile(f)
+			section = elf.get_section_by_name(".dynsym")
+			for symbol in section.iter_symbols():
+				self.symbols[symbol.name] = symbol['st_value']
+
+		with open(path, "rb") as f:
+			self.data = f.read()
 
 	def getSymbol(self, symbol_name):
-		if symbol_name not in self.elf.symbols:
-			raise ValueError(f"Symbol '{symbol_name}' not found in {self.path}")
-		return self.offset + self.elf.symbols[symbol_name]
+		return self.offset + self.symbols[symbol_name]
 
-	def setOffset(self, leaked_address, symbol_name):
-		if symbol_name not in self.elf.symbols:
-			raise ValueError(f"Symbol '{symbol_name}' not found in {self.path}")
-		self.offset = leaked_address - self.elf.symbols[symbol_name]
+	def setOffset(self, address, symbol_name):
+		self.offset = address - self.getSymbol(symbol_name)
 
 	def search(self, byte_sequence):
-		result = next(self.elf.search(byte_sequence), None)
-		if result is None:
-			raise ValueError(f"Byte sequence {byte_sequence} not found in {self.path}")
-		return self.offset + result
+		address = self.data.find(byte_sequence)
+		if address == -1:
+			return 0
+		return self.offset + address
